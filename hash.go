b552b25a9c038e05eece73e056c72997b6c92086
biwow <biwow@qq.com>
2020-11-25 10:09:33 +0800

    调整sum参数

--- hash.go
+++ hash.go
@@ -5,5 +5,7 @@ import "golang.org/x/crypto/sha3"
 func RLPHash(x interface{}) []byte {
 	hw := sha3.NewLegacyKeccak256()
 	Encode(hw, x)
-	return hw.Sum(nil)
+	var h [32]byte
+	hw.Sum(h[:0])
+	return h[:]
 }
